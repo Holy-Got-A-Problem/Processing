3058246fe22d92b55cdc01a301ea8c409dfea9f2
HOLY <65557755+Holy-Got-A-Problem@users.noreply.github.com>
2020-10-03 20:56:50 +0800

    20201003-4

---
+++ p5.js  (VSCode)/Sketch.js
@@ -0,0 +1,21 @@
+class test{
+    constructor(values){
+        this.p = values.p
+        this.clr = values.clr
+    }
+    draw(){
+        fill()
+    }
+
+}
+
+function setup(){
+createCanvas(windowWidth,windowHeight)
+background(0)
+
+}
+
+
+function draw(){
+
+}
